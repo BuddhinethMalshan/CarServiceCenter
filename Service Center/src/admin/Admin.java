bee5aba4b7bad82e32d2446dd6adfa79b6c31144
Sasindu1234 <saindu.rashmina@gmail.com>
2021-09-23 16:28:43 +0530

    Inventoy and admin main

--- Service Center/src/admin/Admin.java
+++ Service Center/src/admin/Admin.java
@@ -8,11 +8,16 @@ import javax.swing.JLabel;
 import java.awt.Color;
 import javax.swing.JPanel;
 import java.awt.Font;
+import javax.swing.ImageIcon;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
 
 
 public class Admin {
 
 	private JFrame frame;
+	
 
 	/**
 	 * Launch the application.
@@ -42,14 +47,30 @@ public class Admin {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 503, 325);
+		frame.setBounds(100, 100, 573, 366);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panelemp = new JPanel();
+		panelemp.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				 panelemp.setBackground(new Color(0,0,204));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panelemp.setBackground(new Color(0, 100, 0));
+			}
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Employee emp = new Employee();
+				//emp.setVisible(true);
+			}
+			
+		});
 		panelemp.setBackground(new Color(0, 100, 0));
 		panelemp.setForeground(Color.WHITE);
-		panelemp.setBounds(133, 62, 120, 40);
+		panelemp.setBounds(251, 62, 120, 40);
 		frame.getContentPane().add(panelemp);
 		
 		JLabel lblemp = new JLabel("\r\nEmployee");
@@ -58,9 +79,19 @@ public class Admin {
 		panelemp.add(lblemp);
 		
 		JPanel panelemp_1 = new JPanel();
+		panelemp_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				 panelemp_1.setBackground(new Color(0,0,204));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panelemp_1.setBackground(new Color(0, 100, 0));
+			}
+		});
 		panelemp_1.setForeground(Color.WHITE);
 		panelemp_1.setBackground(new Color(0, 100, 0));
-		panelemp_1.setBounds(133, 132, 120, 40);
+		panelemp_1.setBounds(251, 132, 120, 40);
 		frame.getContentPane().add(panelemp_1);
 		
 		JLabel lblreport = new JLabel("Report");
@@ -69,9 +100,19 @@ public class Admin {
 		lblreport.setFont(new Font("Tahoma", Font.BOLD, 15));
 		
 		JPanel panelemp_2 = new JPanel();
+		panelemp_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				 panelemp_2.setBackground(new Color(0,0,204));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panelemp_2.setBackground(new Color(0, 100, 0));
+			}
+		});
 		panelemp_2.setForeground(Color.WHITE);
 		panelemp_2.setBackground(new Color(0, 100, 0));
-		panelemp_2.setBounds(274, 62, 120, 40);
+		panelemp_2.setBounds(399, 62, 120, 40);
 		frame.getContentPane().add(panelemp_2);
 		
 		JLabel lblcus = new JLabel("Customer Details");
@@ -80,9 +121,19 @@ public class Admin {
 		panelemp_2.add(lblcus);
 		
 		JPanel panelemp_3 = new JPanel();
+		panelemp_3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				 panelemp_3.setBackground(new Color(0,0,204));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panelemp_3.setBackground(new Color(0, 100, 0));
+			}
+		});
 		panelemp_3.setForeground(Color.WHITE);
 		panelemp_3.setBackground(new Color(0, 100, 0));
-		panelemp_3.setBounds(274, 132, 120, 40);
+		panelemp_3.setBounds(399, 132, 120, 40);
 		frame.getContentPane().add(panelemp_3);
 		
 		JLabel lblServices = new JLabel("Services");
@@ -91,9 +142,23 @@ public class Admin {
 		panelemp_3.add(lblServices);
 		
 		JPanel panelemp_4 = new JPanel();
+		panelemp_4.addMouseListener(new MouseAdapter(){
+	    public void mouseClicked(MouseEvent e) {
+	        System.out.println("Clicked");
+	    }
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				 panelemp_4.setBackground(new Color(0,0,204));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panelemp_4.setBackground(new Color(0, 100, 0));
+			}
+	});
 		panelemp_4.setForeground(Color.WHITE);
 		panelemp_4.setBackground(new Color(0, 100, 0));
-		panelemp_4.setBounds(133, 202, 120, 40);
+		panelemp_4.setBounds(251, 202, 120, 40);
 		frame.getContentPane().add(panelemp_4);
 		
 		JLabel lblItems = new JLabel("Items");
@@ -102,15 +167,33 @@ public class Admin {
 		panelemp_4.add(lblItems);
 		
 		JPanel panelemp_5 = new JPanel();
+		panelemp_5.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				 panelemp_5.setBackground(new Color(0,0,204));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panelemp_5.setBackground(new Color(0, 100, 0));
+			}
+		});
 		panelemp_5.setForeground(Color.WHITE);
 		panelemp_5.setBackground(new Color(0, 100, 0));
-		panelemp_5.setBounds(274, 202, 120, 40);
+		panelemp_5.setBounds(399, 202, 120, 40);
 		frame.getContentPane().add(panelemp_5);
 		
 		JLabel lblSetting = new JLabel("Settings");
 		lblSetting.setForeground(Color.WHITE);
 		lblSetting.setFont(new Font("Tahoma", Font.BOLD, 15));
 		panelemp_5.add(lblSetting);
+		
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(Admin.class.getResource("/image/Customer.jpg")));
+		lblNewLabel.setBounds(0, 0, 557, 327);
+		frame.getContentPane().add(lblNewLabel);
+		
+		
 	}
 }
 
--- Service Center/src/admin/Inventory.java
+++ Service Center/src/admin/Inventory.java
@@ -3,10 +3,34 @@ package admin;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.awt.event.ActionEvent;
 
 public class Inventory {
 
 	private JFrame frame;
+	private JTable table;
+	private JTextField txtqty;
+	private JTextField txtprice;
+	private JTextField txtname;
+	private Object[] row ;
+	
+	
+	private DefaultTableModel model;
 
 	/**
 	 * Launch the application.
@@ -36,8 +60,132 @@ public class Inventory {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 612, 376);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(172, 0, 420, 195);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 0, 420, 195);
+		panel.add(scrollPane);
+		
+		table = new JTable();
+		model = new DefaultTableModel();
+		Object[] columns = {"Item ID" , "Item name" , "Item Quantity" ,"Item Price"};
+		model.setColumnIdentifiers(columns);
+		table.setAutoCreateRowSorter(true);
+		table.setModel(model);
+
+		row = new Object[4];
+		 try {  
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection con=DriverManager.getConnection("jdbc:mysql://localhost/carservice", "root", "");
+			    Statement stmt = con.createStatement();				
+			    String sql="SELECT * FROM `item` WHERE 1";
+				ResultSet rs=stmt.executeQuery(sql);
+				while (rs.next()) {
+					
+					row[0] = rs.getString("item_id");
+					row[1] = rs.getString("Item_name");
+					row[2] = rs.getString("item_qty");
+					row[3] = rs.getString("item_price");
+					model.addRow(row);
+					}
+				con.close();
+			 } 			
+		 catch(Exception e1) {
+				System.out.print(e1);
+			 }
+	
+		table.getColumnModel().getColumn(0).setPreferredWidth(6);
+		table.getColumnModel().getColumn(3).setPreferredWidth(10);
+		scrollPane.setViewportView(table);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(102, 205, 170));
+		panel_1.setBounds(0, 0, 173, 337);
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Item Name");
+		lblNewLabel.setForeground(new Color(0, 0, 0));
+		lblNewLabel.setBounds(10, 22, 80, 14);
+		panel_1.add(lblNewLabel);
+		
+		JLabel lbqty = new JLabel("Item Quantity");
+		lbqty.setBounds(10, 79, 80, 14);
+		panel_1.add(lbqty);
+		
+		JLabel lblprice = new JLabel("Item Price");
+		lblprice.setBounds(10, 135, 80, 14);
+		panel_1.add(lblprice);
+		
+		txtqty = new JTextField();
+		txtqty.setBounds(77, 104, 86, 20);
+		panel_1.add(txtqty);
+		txtqty.setColumns(10);
+		
+		txtprice = new JTextField();
+		txtprice.setBounds(77, 160, 86, 20);
+		panel_1.add(txtprice);
+		txtprice.setColumns(10);
+		
+		txtname = new JTextField();
+		txtname.setBounds(77, 47, 86, 20);
+		panel_1.add(txtname);
+		txtname.setColumns(10);
+		
+		JButton btnnewitem = new JButton("New Item");
+		btnnewitem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				insert() ;
+			}
+		});
+		btnnewitem.setBounds(74, 222, 99, 23);
+		panel_1.add(btnnewitem);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(0, 139, 139));
+		panel_2.setBounds(172, 192, 424, 145);
+		frame.getContentPane().add(panel_2);
+		panel_2.setLayout(null);
 	}
+	private void insert() {
+		   int itemqty = 0;
+		   int itemprice = 0;
+		   String itemname;
+		   
+		   
+		   try {
+			   
+			   itemqty = Integer.parseInt(txtqty.getText());
+			   itemname = txtname.getText();
+			   itemprice = Integer.parseInt(txtprice.getText());
+	       	    Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection con=DriverManager.getConnection("jdbc:mysql://localhost/carservice", "root", "");
+				 String query1 = "INSERT INTO `item`( `Item_name`, `item_qty`, `item_price`) VALUES ('"+itemname +"',"+itemqty+","+itemprice+")";
+	             Statement ps = con.prepareStatement(query1);
+	             ps.executeUpdate(query1);
+	             
+	             JOptionPane.showMessageDialog(null, "Item Information Update");
+	            
+	             
+	             
+	             
+	             
+	             
+	             
+	             con.close();
+	           
+	       } catch (Exception e) {
+	           JOptionPane.showMessageDialog(null,  e.getMessage());
+	       }
+	   }
+	   
+
 
 }

ac710aa9a8b4805f05805252f2976c0ee12510d9
LAPTOP-48BL5AJN\user <buddhinethmalshan@gmail.com>
2021-09-24 19:59:53 +0530

    Home with background design.(Title not finalized)

--- Service Center/src/Home.java
+++ Service Center/src/Home.java
@@ -8,8 +8,10 @@ import javax.swing.JOptionPane;
 
 import java.awt.Font;
 import java.sql.*;
+import java.util.Timer;
 
 import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JRadioButton;
@@ -17,16 +19,31 @@ import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Component;
+import java.awt.Point;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import java.awt.SystemColor;
 
 
 
 public class Home {
 
-	private JFrame frame;
+	private JFrame frmHome;
 	private JTextField txtname;
 	private JPasswordField txtpass;
-	//private JRadioButton rbtnAdmin,rbtnCashier;
 	private final ButtonGroup bg1 = new ButtonGroup();
+	
+	private JLabel lblslide;
+	Timer tm;
+	int x=0;
+	String[] list = { "/images/carService0.jpeg",
+					  "/images/carService1.jpeg",
+					  "/images/carService2.jpeg",
+					  "/images/carService3.jpeg",
+					  "/images/carService4.jpeg",
+					  "/images/carService5.jpeg",
+	};
 
 
 	/**
@@ -37,7 +54,7 @@ public class Home {
 			public void run() {
 				try {
 					Home window = new Home();
-					window.frame.setVisible(true);
+					window.frmHome.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -50,60 +67,68 @@ public class Home {
 	 */
 	public Home() {
 		initialize();
+		
+
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 1083, 694);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmHome = new JFrame();
+		frmHome.setTitle("Home");
+		frmHome.setBounds(100, 100, 1083, 694);
+		frmHome.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmHome.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("User Login");
+		lblNewLabel.setForeground(new Color(0, 0, 0));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 25));
-		lblNewLabel.setBounds(850, 146, 184, 37);
-		frame.getContentPane().add(lblNewLabel);
+		lblNewLabel.setBounds(830, 160, 184, 37);
+		frmHome.getContentPane().add(lblNewLabel);
 		
 		JLabel lblname = new JLabel("User Name");
 		lblname.setHorizontalAlignment(SwingConstants.CENTER);
 		lblname.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblname.setBounds(778, 220, 105, 29);
-		frame.getContentPane().add(lblname);
+		lblname.setBounds(791, 253, 105, 29);
+		frmHome.getContentPane().add(lblname);
 		
 		JLabel lblpass = new JLabel("Password");
 		lblpass.setHorizontalAlignment(SwingConstants.CENTER);
 		lblpass.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblpass.setBounds(778, 271, 105, 29);
-		frame.getContentPane().add(lblpass);
+		lblpass.setBounds(791, 331, 105, 29);
+		frmHome.getContentPane().add(lblpass);
 		
 		txtname = new JTextField();
-		txtname.setBounds(893, 220, 130, 28);
-		frame.getContentPane().add(txtname);
+		txtname.setBounds(906, 256, 130, 28);
+		frmHome.getContentPane().add(txtname);
 		txtname.setColumns(10);
 		
 		txtpass = new JPasswordField();
-		txtpass.setBounds(893, 270, 130, 28);
-		frame.getContentPane().add(txtpass);
+		txtpass.setBounds(908, 330, 130, 28);
+		frmHome.getContentPane().add(txtpass);
 		
 		JRadioButton rbtnAdmin = new JRadioButton("Admin");
+		rbtnAdmin.setBackground(SystemColor.activeCaption);
 		bg1.add(rbtnAdmin);
 		rbtnAdmin.setFont(new Font("Tahoma", Font.BOLD, 13));
-		rbtnAdmin.setBounds(790, 332, 67, 21);
+		rbtnAdmin.setBounds(810, 421, 67, 21);
 		rbtnAdmin.setActionCommand("Admin");
-		frame.getContentPane().add(rbtnAdmin);
+		frmHome.getContentPane().add(rbtnAdmin);
 		
 		JRadioButton rbtnCashier = new JRadioButton("Cashier");
+		rbtnCashier.setBackground(SystemColor.activeCaption);
 		rbtnCashier.setSelected(true);
 		bg1.add(rbtnCashier);
 		rbtnCashier.setFont(new Font("Tahoma", Font.BOLD, 13));
-		rbtnCashier.setBounds(860, 332, 75, 21);
+		rbtnCashier.setBounds(885, 421, 75, 21);
 		rbtnCashier.setActionCommand("Cashier");
-		frame.getContentPane().add(rbtnCashier);
+		frmHome.getContentPane().add(rbtnCashier);
 		
 		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setBackground(Color.DARK_GRAY);
+		btnNewButton.setForeground(SystemColor.textHighlightText);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -119,8 +144,43 @@ public class Home {
 			}
 		});
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 14));
-		btnNewButton.setBounds(938, 327, 87, 32);
-		frame.getContentPane().add(btnNewButton);
+		btnNewButton.setBounds(966, 414, 93, 32);
+		frmHome.getContentPane().add(btnNewButton);	
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		try {
+		lblNewLabel_2.setIcon(new ImageIcon(this.getClass().getResource("/images/Car_logos.png")));
+		}catch(NullPointerException e) {
+			System.out.println(e);
+		}
+		lblNewLabel_2.setBounds(447, 597, 600, 50);
+		frmHome.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_1 = new JLabel("<html><body>4 Wheel Auto Service<br>32/B,<br>Queen's Road,<br>Colombo 7</body></html>");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_1.setOpaque(true);
+		lblNewLabel_1.setForeground(Color.WHITE);
+		lblNewLabel_1.setBackground(Color.DARK_GRAY);
+		lblNewLabel_1.setBorder(new EmptyBorder(0,30,0,0));
+		lblNewLabel_1.setBounds(0, 512, 1069, 145);
+		frmHome.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblslide = new JLabel("");
+		lblslide.setBounds(0, 134, 794, 379);
+		frmHome.getContentPane().add(lblslide);
+		lblslide.setIcon(new ImageIcon(this.getClass().getResource("/images/slides.gif")));
+		
+		JLabel lblloginBack = new JLabel("");
+		lblloginBack.setBackground(SystemColor.activeCaption);
+		lblloginBack.setOpaque(true);
+		lblloginBack.setBounds(791, 134, 278, 379);
+		frmHome.getContentPane().add(lblloginBack);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		lblNewLabel_3.setBounds(0, 0, 1069, 137);
+		frmHome.getContentPane().add(lblNewLabel_3);
+		
+		
 	}
 	
 	private void login() {
@@ -138,7 +198,7 @@ public class Home {
 		
 		try {
 			Connection conn1= DriverManager.getConnection("jdbc:mysql://localhost:3306/CarService","root","12345");
-	        System.out.println("rrrrrrr");
+			
 			if(bg1.getSelection().getActionCommand()=="Admin") {
 				//System.out.println("rrrrrrr");
 				ps1 = conn1.prepareStatement(sql1);
@@ -179,10 +239,6 @@ public class Home {
 		
 		
 	}
-	
-	
 }
-
-
 
 

066a022aff8c8c899e5ec4d5284e9cd64a79073a
LAPTOP-48BL5AJN\user <buddhinethmalshan@gmail.com>
2021-09-26 12:25:35 +0530

    JFrame name changed from frmHome to frame

--- Service Center/src/Home.java
+++ Service Center/src/Home.java
@@ -29,7 +29,7 @@ import java.awt.SystemColor;
 
 public class Home {
 
-	private JFrame frmHome;
+	private JFrame frame;
 	private JTextField txtname;
 	private JPasswordField txtpass;
 	private final ButtonGroup bg1 = new ButtonGroup();
@@ -42,7 +42,7 @@ public class Home {
 			public void run() {
 				try {
 					Home window = new Home();
-					window.frmHome.setVisible(true);
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -63,39 +63,39 @@ public class Home {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmHome = new JFrame();
-		frmHome.setTitle("Home");
-		frmHome.setBounds(100, 100, 1083, 694);
-		frmHome.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmHome.getContentPane().setLayout(null);
+		frame = new JFrame();
+		frame.setTitle("Home");
+		frame.setBounds(100, 100, 1083, 694);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("User Login");
 		lblNewLabel.setForeground(new Color(0, 0, 0));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 25));
 		lblNewLabel.setBounds(830, 160, 184, 37);
-		frmHome.getContentPane().add(lblNewLabel);
+		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblname = new JLabel("User Name");
 		lblname.setHorizontalAlignment(SwingConstants.CENTER);
 		lblname.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblname.setBounds(791, 253, 105, 29);
-		frmHome.getContentPane().add(lblname);
+		frame.getContentPane().add(lblname);
 		
 		JLabel lblpass = new JLabel("Password");
 		lblpass.setHorizontalAlignment(SwingConstants.CENTER);
 		lblpass.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblpass.setBounds(791, 331, 105, 29);
-		frmHome.getContentPane().add(lblpass);
+		frame.getContentPane().add(lblpass);
 		
 		txtname = new JTextField();
 		txtname.setBounds(906, 256, 130, 28);
-		frmHome.getContentPane().add(txtname);
+		frame.getContentPane().add(txtname);
 		txtname.setColumns(10);
 		
 		txtpass = new JPasswordField();
 		txtpass.setBounds(908, 330, 130, 28);
-		frmHome.getContentPane().add(txtpass);
+		frame.getContentPane().add(txtpass);
 		
 		JRadioButton rbtnAdmin = new JRadioButton("Admin");
 		rbtnAdmin.setBackground(SystemColor.activeCaption);
@@ -103,7 +103,7 @@ public class Home {
 		rbtnAdmin.setFont(new Font("Tahoma", Font.BOLD, 13));
 		rbtnAdmin.setBounds(810, 421, 67, 21);
 		rbtnAdmin.setActionCommand("Admin");
-		frmHome.getContentPane().add(rbtnAdmin);
+		frame.getContentPane().add(rbtnAdmin);
 		
 		JRadioButton rbtnCashier = new JRadioButton("Cashier");
 		rbtnCashier.setBackground(SystemColor.activeCaption);
@@ -112,7 +112,7 @@ public class Home {
 		rbtnCashier.setFont(new Font("Tahoma", Font.BOLD, 13));
 		rbtnCashier.setBounds(885, 421, 75, 21);
 		rbtnCashier.setActionCommand("Cashier");
-		frmHome.getContentPane().add(rbtnCashier);
+		frame.getContentPane().add(rbtnCashier);
 		
 		JButton btnNewButton = new JButton("Login");
 		btnNewButton.setBackground(Color.DARK_GRAY);
@@ -133,7 +133,7 @@ public class Home {
 		});
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 14));
 		btnNewButton.setBounds(966, 414, 93, 32);
-		frmHome.getContentPane().add(btnNewButton);	
+		frame.getContentPane().add(btnNewButton);	
 		
 		JLabel lblNewLabel_2 = new JLabel("");
 		try {
@@ -142,7 +142,7 @@ public class Home {
 			System.out.println(e);
 		}
 		lblNewLabel_2.setBounds(447, 597, 600, 50);
-		frmHome.getContentPane().add(lblNewLabel_2);
+		frame.getContentPane().add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_1 = new JLabel("<html><body>4 Wheel Auto Service<br>32/B,<br>Queen's Road,<br>Colombo 7</body></html>");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
@@ -151,22 +151,22 @@ public class Home {
 		lblNewLabel_1.setBackground(Color.DARK_GRAY);
 		lblNewLabel_1.setBorder(new EmptyBorder(0,30,0,0));
 		lblNewLabel_1.setBounds(0, 512, 1069, 145);
-		frmHome.getContentPane().add(lblNewLabel_1);
+		frame.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblslide = new JLabel("");
 		lblslide.setBounds(0, 134, 794, 379);
-		frmHome.getContentPane().add(lblslide);
+		frame.getContentPane().add(lblslide);
 		lblslide.setIcon(new ImageIcon(this.getClass().getResource("/images/slides.gif")));
 		
 		JLabel lblloginBack = new JLabel("");
 		lblloginBack.setBackground(SystemColor.activeCaption);
 		lblloginBack.setOpaque(true);
 		lblloginBack.setBounds(791, 134, 278, 379);
-		frmHome.getContentPane().add(lblloginBack);
+		frame.getContentPane().add(lblloginBack);
 		
 		JLabel lblNewLabel_3 = new JLabel("New label");
 		lblNewLabel_3.setBounds(0, 0, 1069, 137);
-		frmHome.getContentPane().add(lblNewLabel_3);
+		frame.getContentPane().add(lblNewLabel_3);
 		
 		
 	}
